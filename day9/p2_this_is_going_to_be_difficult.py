fd9175528438ef6943a86faf0de2e606ba871f93
Pedrochem <pedrochem@hotmail.com>
2024-01-19 22:02:10 -0300

    for p2 too

--- day9/p2_this_is_going_to_be_difficult.py
+++ day9/p2_this_is_going_to_be_difficult.py
@@ -1,14 +1,22 @@
 
 with open("full_input.txt") as f:
     lines = [line.rstrip() for line in f]
-    next_value = 0
-    for line in lines:
+    res=0
+    for line in lines[:]:
         hist = [int(x) for x in line.split()]
         new_seq = hist
-        next_value += hist[-1]
+        next_values = [hist[0]]
+        # print(next_values)
         while (set(new_seq) != set([0])):
             new_seq =  [y-x for x,y in zip(new_seq[:-1],new_seq[1:])]
-            next_value += new_seq[-1]
+            next_values.append(new_seq[0])
+            # print(new_seq[0])
 
-    print(next_value)
+        next_val = 0
+        for v in next_values[::-1][1:]:
+            next_val = v - next_val
+            # print(v)
         
+        # print(next_val)
+        res+=next_val
+    print(res)
