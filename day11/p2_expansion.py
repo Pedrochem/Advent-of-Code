7aea15d37a53c4709e2e48fd9d2f89a7516e2c0d
Pedrochem <pedrochem@hotmail.com>
2024-01-27 02:11:27 -0300

    added p2 day11

--- day11/p2_expansion.py
+++ day11/p2_expansion.py
@@ -7,8 +7,7 @@ def expand(lines):
     grid = np.array([list(line.rstrip()) for line in lines])
     grid_transpose = grid.T 
 
-
-    print(grid)
+    # print(grid)
     print()
     print()
     # print(grid_transpose)
@@ -24,37 +23,38 @@ def expand(lines):
         if '#' not in column:
             columns_to_expand.append(j)
 
-    # print(rows_to_expand)
-    # print(columns_to_expand) 
-
-    grid_expanded = np.insert(grid,rows_to_expand,['.'*len(grid)],axis=0)
-    grid_expanded = np.insert(grid_expanded,columns_to_expand,'.'*len(grid_expanded[0]),axis=1)
-
-    print(grid_expanded)
-    return grid_expanded
+    return rows_to_expand, columns_to_expand
 
 
-with open("input.txt") as f:
-    grid = expand(f.readlines())
+with open("full_input.txt") as f:
+    lines = f.readlines()
+    grid = np.array([list(line.rstrip()) for line in lines])
+    extra_rows, extra_columns = expand(lines)
+    print(extra_rows)
+
     galaxies = []
     for i,row in enumerate(grid):
         for j,val in enumerate(row):
             if val == '#':
                 galaxies.append((i,j))
 
-    print(galaxies)
 
     distances = []
     for x,(curr_i,curr_j) in enumerate(galaxies):
-        print(f'Galaxy:{x}: {(curr_i,curr_j)}')
+        # print(f'Galaxy:{x}: {(curr_i,curr_j)}')
         y = x+1
         
         while y<len(galaxies):
             gal_i, gal_j = galaxies[y]
             dist = abs(curr_i-gal_i) + abs(curr_j-gal_j)
-            print(f'Dist galaxy {x} and {y}: {dist}')
+            for row in extra_rows:
+                if curr_i > row > gal_i or gal_i > row > curr_i:
+                    dist+=999999
+            for column in extra_columns:
+                if curr_j > column > gal_j or gal_j > column > curr_j:
+                    dist+=999999
+            # print(f'Dist galaxy {x} and {y}: {dist}')
+        
             distances.append(dist)
             y+=1
-        break
-        
     print(sum(distances))
